f06769b7e3bf71c82dfbe80ed81db2ed3bb0efbd
DEVIKASAJI <sajidevika243@gmail.com>
2025-01-25 19:50:01 +0530

    searchfield

--- photodashboard/photoapp/views.py
+++ photodashboard/photoapp/views.py
@@ -1,3 +1,14 @@
+
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def search(request):
+    query = request.GET.get('query', '')
+   
+    context = {
+        'query': query,
+    }
+    return render(request, 'search_results.html', context)  
